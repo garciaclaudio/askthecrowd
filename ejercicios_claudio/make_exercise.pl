1f20cb09055455f2762f659585eb9eec5223401c
Claudio Garcia <claudio.garcia@booking.com>
2012-07-19 00:21:37 +0200

    WIP

--- ejercicios_claudio/make_exercise.pl
+++ ejercicios_claudio/make_exercise.pl
@@ -8,35 +8,174 @@ use Tk::JPEG;
 use Data::Dumper;
 use List::Util qw( shuffle );
 
-BEGIN {
-#    $ENV{HTTP_proxy}='http://webproxy.corp.booking.com:3128/';
-}
-
-#
-# VER: man htmldoc
-#
-#
-# vi /usr/local/bin/tkjpeg
-# vi /usr/local/bin/widgets
-#
-# usage:
-#
-# perl test_mechanize.pl < word_list2 > /tmp/test2.html
-# cat /tmp/test2.html | htmldoc -f /tmp/list3.pdf - --webpage --size a4 --no-numbered --fontsize 18
-#
-#  perl test_mechanize.pl < ~/BKP/EJERCICIOS_CLAUDIO/lista3 | htmldoc -f ~/BKP/EJERCICIOS_CLAUDIO/chavo_del_ocho.pdf - --webpage --size a4 --no-numbered --fontsize 18
-#
-# LATEST FORMAT:
-#
-# perl test_mechanize.pl -pdf_name=output_files/test_wl_16 < word_lists/word_list_16
-#
-#
-
-my @word_list = grep{ $_ } map { chomp $_; $_ } (<STDIN>);
-
 use LWP::Simple;
 use Getopt::Long;
 use WWW::Mechanize;
+use DBD::SQLite;
+use DBI qw(:sql_types);
+
+my $dbfile = '/home/claudio/Documents/data.db3';
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","","");
+
+#
+# perl make_exercise.pl
+#
+
+
+my %images;
+
+my $rows = $dbh->selectall_arrayref("SELECT id,tag,group_tag,image FROM images");
+
+foreach my $row ( @$rows ) {
+    my( $id, $tag, $group_tag, $image) = @$row;
+    push @{ $images{$tag} }, { id => $id, group_tag => $group_tag, image_blob => $image };
+}
+
+
+my $mw  = MainWindow->new();
+
+my $old_frame;
+
+sub make_sub {
+    my $c = shift;
+
+    return sub{ return $c };
+};
+
+
+sub add_new_frame {
+
+    my $new_frame = $mw->Frame();
+
+    # XXX, fix this
+    my ($selected_tag) = (keys %images);
+
+    my @selected_images = @{ $images{$selected_tag} };
+
+    my $selected = -1;
+
+    my(@pl) = qw/-side top -padx .5m -pady .5m/;
+
+    my(@left) = qw/-side left -padx .5m -pady .5m/;
+
+    $new_frame->Label( -text => $selected_tag )->pack(@pl);
+
+    $new_frame->Button(
+		-text    => "mostrar otras",
+		-width   => 10,
+		-command => sub {
+				  $old_frame->destroy;
+				  add_new_frame( $mw );
+			      },
+	       )->pack(@pl);
+
+    while( my @doing = splice( @selected_images, 0, 2 ) ) {
+
+	my $inner_frame = $new_frame->Frame()->pack(@left);
+
+	foreach my $img (@doing) {
+            my $label = 'image' . $img->{id};
+            open FOO, ">/tmp/$label.jpg";
+            print FOO $img->{image_blob};
+            close FOO;
+	    $inner_frame->Photo( $label, -file => "/tmp/$label.jpg" );
+	    $inner_frame->Label( -image => $label )->pack(@pl);
+
+	    my $subby = make_sub( $img->{id} );
+
+	    $inner_frame->Button(
+			       -text    => "seleccióname",
+			       -width   => 10,
+			       -command => sub { $selected = &$subby;
+#				      print "SELECTED $selected\n";
+#						 $selected_images{$active_word} = $selected;
+#
+#						 if( scalar keys %selected_images == 18 ) {
+#
+#						     make_pdf( \%selected_images );
+#
+#						     %selected_images = ();
+#						 }
+#
+#						 next_word();
+						 $old_frame->destroy;
+						 add_new_frame( $mw );
+					     },
+			      )->pack(@pl);
+	}
+    }
+
+    $new_frame->pack();
+
+    $mw->minsize( $mw->width(),
+		  $mw->height() );
+
+    $old_frame = $new_frame;
+}
+
+add_new_frame( $mw );
+
+MainLoop;
+
+
+
+__END__
+
+
+foreach my $word ( @word_list ) {
+    my @images = get_images( $words_file, $word );
+}
+
+
+
+
+
+sub get_images {
+    my ($words_file, $word) = @_;
+
+    my $mech = WWW::Mechanize->new();
+
+    $mech->get( 'http://images.google.com' );
+
+    $mech->submit_form(
+        form_number => 1,
+        fields      => { q => $word }
+    );
+
+    my @imgs = grep { $_->url =~ /gstatic/ } @{ $mech->images };
+
+    my @doing =  splice @imgs, 0, 10;
+
+    my @result;
+
+    my $img_index = 0;
+
+    foreach my $i (@doing) {
+	my $url = $i->url;	
+#	print STDERR "URL: $url\n";
+	next if( $url !~ /gstatic/ );
+
+	my $img = get $url;
+
+        print "Inserting $word\n";
+
+        my $sth = $dbh->prepare("INSERT INTO images (tag, group_tag, image) VALUES (?,?,?)");
+        $sth->bind_param(1, $word);
+        $sth->bind_param(2, $words_file);
+        $sth->bind_param(3, $img, SQL_BLOB);
+        $sth->execute();
+    }
+
+    return @result;
+}
+
+
+
+__END__
+
+
+
 
 #my $mech = WWW::Mechanize->new();
 
