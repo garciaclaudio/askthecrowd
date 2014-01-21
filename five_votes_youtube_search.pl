b9d9e8db58103539c0f05eabb27fdab41d9fe6cd
Claudio Garcia <claudio.garcia@booking.com>
2014-01-21 20:03:20 +0100

    WIP

--- five_votes_youtube_search.pl
+++ five_votes_youtube_search.pl
@@ -6,13 +6,19 @@ use WebService::GData::YouTube;
  
 my $yt = new WebService::GData::YouTube();
  
-$yt->query()->q("Chico Che")->limit(50,0);
+$yt->query()->q("Chico Che")->limit(3,0);
  
 #or set your own query object
 #$yt->query($myquery);
  
 my $videos = $yt->search_video();
 
+#print Dumper( $videos );
+#__END__
+
+#
+# XXX, check for embed permission?
+#
 #
 # XXX, discard too recent (may be struck down soon)?
 # Better implement cron job later to automatically clean up deceased videos
@@ -51,12 +57,19 @@ grep { $_->genre eq 'Music' }
 foreach my $v ( @sorted ) {
 
     my $title = $v->title;
+    my $video_id = $v->video_id;
     my $duration = $v->duration;
+    my $description = $v->description;
     my $view_count = $v->view_count;
     my $num_likes = $v->rating->num_likes;
     my $num_dislikes = $v->rating->num_dislikes;
     my $genre = $v->genre;
     my $score = ( $num_likes - $num_dislikes ) / $view_count;
+    my $category = $v->category;
+    my $uploaded = $v->uploaded;
+
+#    my $location = $v->location;
+#    my $keywords = $v->keywords;
 
     print "TITLE: $title\n";
     print "Score: $score\n";
@@ -64,10 +77,12 @@ foreach my $v ( @sorted ) {
     print "Views: $view_count\n";
     print "Num likes: $num_likes\n";
     print "Num dislikes: $num_dislikes\n";
-#    print Dumper( $rating );
+    print "video id: $video_id\n";
+    print "Desc: $description\n";
 
+    print Dumper( $uploaded );
     print "Genre: $genre\n\n";
 }
 
 
-#print STDERR Dumper( $videos );
+
